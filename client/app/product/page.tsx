ad600e1ddbc1bf0e41e8bd78cbfa0e26a26e2033
AymenBraikia <aymenbraikia1@gmail.com>
2025-05-29 20:25:38 +0100

    x

--- client/app/product/page.tsx
+++ client/app/product/page.tsx
@@ -1,13 +1,16 @@
-"use client"
+"use client";
 // import "./responsive.css";
 import Header from "../components/header";
 import Body from "./components/body";
+import { Suspense } from "react";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
-			<Body />
+			<Suspense fallback={<div>Loading...</div>}>
+				<Body />
+			</Suspense>
 		</>
 	);
 }
