0d388a2a750cc0a8e09408c3766eab51f61cb19b
AymenBraikia <aymenbraikia1@gmail.com>
2025-06-02 14:55:39 +0100

    Update layout.tsx

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-// import { SpeedInsights } from "@vercel/speed-insights/next"
-import "@vercel/speed-insights/next";
+
 
 const myCustomFont = localFont({
 	src: [
